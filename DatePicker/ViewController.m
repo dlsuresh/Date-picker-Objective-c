0b595b88c11abbe23bebb2dde111ef5c205df6e5
dlsuresh <dl.suresh6@gmail.com>
2017-03-07 15:47:05 +0530

    initial commit

--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -16,7 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //self.datePicker.datePickerMode = UIDatePickerModeDate;
+    
+    [self.datePicker setMaximumDate:[NSDate date]];
+    
+    
+    [self.datePicker addTarget:self action:@selector(textFieldUpdate) forControlEvents:UIControlEventValueChanged];
+    
+    [self.txtField setInputView:self.datePicker];
+    
+    self.datePicker.hidden = YES;
+    
+    NSDateFormatter *dateformat = [[NSDateFormatter alloc]init];
+    
+    [dateformat setDateStyle:NSDateFormatterMediumStyle];
+    
+    [dateformat setDateFormat:@"YYYY-MM-dd"];
+    
+    
+    self.txtField.text = [NSString stringWithFormat:@"%@",[dateformat stringFromDate:[NSDate date]]];
+    self.todayDate.text = [NSString stringWithFormat:@"%@",[dateformat stringFromDate:[NSDate date]]];
+
 }
 
 
@@ -25,5 +45,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    
+    self.datePicker.hidden = NO;
+    
+    
+    [textField resignFirstResponder];
+    return NO;
+}
+-(void)textFieldUpdate{
+    //self.datePicker = (UIDatePicker*)self.txtField.inputView;
+    
+    
+    NSDateFormatter *format = [[NSDateFormatter alloc]init];
+    [format setDateFormat:@"YYYY-MM-dd"];
+    
+    self.txtField.text = [NSString stringWithFormat:@"%@",[format stringFromDate:self.datePicker.date]];
+    
+}
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.datePicker.hidden = YES;
+}
 
 @end
